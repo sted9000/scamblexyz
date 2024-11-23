d82f7436b931ef1931f9dae36979843e654ee3e6
sted9000 <sted9000@gmail.com>
2024-11-23 13:07:25 -0500

    www continued

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,7 +15,7 @@ router.post("/google", async (req, res) => {
   const oAuth2Client = new OAuth2Client({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    redirectUri: "https://scamble.xyz/auth/google/",
+    redirectUri: "https://www.scamble.xyz/auth/google/",
   });
 
   try {
